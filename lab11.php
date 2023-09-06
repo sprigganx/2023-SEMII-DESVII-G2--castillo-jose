a5ad902c87c2b004cd7dddfe6776704d9a1571ae
[Jose Castillo] <[jrogelioc191@gmail.com]>
2023-09-06 03:11:15 -0500

    lab1

---
+++ lab11.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>laboratorio 1.1</title>
+</head>
+
+<body>
+    <?php
+    $ini = "Hola";
+    $fin = " a todos";
+    $todo = $ini . $fin;
+    echo $todo;
+    ?>
+</body>
+
+</html>
