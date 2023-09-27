5eae88413fb3b261d2e05f4f0807c14d03cca096
[Jose Castillo] <[jrogelioc191@gmail.com]>
2023-09-27 15:44:14 -0500

    laboratorio

--- lab7/class_lib.php
+++ lab7/class_lib.php
@@ -193,5 +193,30 @@
         }
     }
 
+    class Cilindro{
+        protected $pi;
+        protected $diametro;
+        protected $altura;
+        protected $radio;
+
+        function __construct($d, $a){
+            $this->diametro = $d;
+            $this->altura = $a; 
+            $this->pi = 3.141593;
+            $this->radio=$d/2;
+        }
+
+        function obtener_radio(){
+            return $radio;
+        }
+
+        function calc_volumen(){
+            return $this->pi*$this->radio*$this->radio*$this->altura;
+        }
+
+        function calc_area(){
+            return 2*$this->pi*$this->radio*($this->radio+$this->altura);
+        }
+    }
 
 ?>
